a3a294b61e2d1795a0ce452fc10ee3f51856f8ab
Marco Vinicius de Melo Faria <marco.melo-faria@unesp.br>
2023-11-04 01:21:58 -0300

    alguns ajustes e adições de algoritmos

---
+++ InsertionSort.py
@@ -0,0 +1,9 @@
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        chave = arr[i]
+        j = i - 1
+        while j >= 0 and chave < arr[j]:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = chave
+
---
+++ MergeSort.py
@@ -0,0 +1,29 @@
+def merge_sort(arr):
+    if len(arr) > 1:
+        meio = len(arr) // 2
+        metade_esquerda = arr[:meio]
+        metade_direita = arr[meio:]
+
+        merge_sort(metade_esquerda)
+        merge_sort(metade_direita)
+
+        i = j = k = 0
+
+        while i < len(metade_esquerda) and j < len(metade_direita):
+            if metade_esquerda[i] < metade_direita[j]:
+                arr[k] = metade_esquerda[i]
+                i += 1
+            else:
+                arr[k] = metade_direita[j]
+                j += 1
+            k += 1
+
+        while i < len(metade_esquerda):
+            arr[k] = metade_esquerda[i]
+            i += 1
+            k += 1
+
+        while j < len(metade_direita):
+            arr[k] = metade_direita[j]
+            j += 1
+            k += 1
